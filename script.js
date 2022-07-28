f1acb798a7b955cc44a93adc33fcbece42a7e47c
Norali Luna <luna.norali422@gmail.com>
2022-07-27 22:44:09 -0300

    Function

--- script.js
+++ script.js
@@ -16,7 +16,6 @@ for (let i = 0; i < productos; i++){
     console.log(contador)
 }
 
-
 let tarjeta = prompt("Ingrese su tarjeta").toLowerCase();
 
 const DESC1 = 0.3;
@@ -24,24 +23,31 @@ const DESC2 = 0.2;
 const DESC3 = 0.15;
 
 
+function calcularDescuento(descuento){
+    return acumulador*descuento;
+}
+function total(a){
+    return acumulador - a;
+}
+
 if (tarjeta == "santander"){
     if (acumulador >= 3000){
-        let descuento1 = acumulador*DESC1;
-        console.log ("El total de su compra es: $" + (acumulador) + ". Se le descuenta el 30%, abonará en total $" + (acumulador-descuento1));
+        calcularDescuento(DESC1);
+        console.log ("El total de su compra es: $" + (acumulador) + ". Se le descuenta el 30%, abonará en total $" + (total(calcularDescuento(DESC1))));
     }else{
         console.log("No se le efectura el descuento.")
     }
 } else if (tarjeta == "ciudad"){
     if (acumulador >= 1000){
-        let descuento2 = acumulador*DESC2;
-        console.log ("El total de su compra es: $" + (acumulador) + ". Se le descuenta el 20%, abonará en total $" + (acumulador-descuento2));
+        calcularDescuento(DESC2);
+        console.log ("El total de su compra es: $" + (acumulador) + ". Se le descuenta el 20%, abonará en total $" + (total(calcularDescuento(DESC2))));
     }else{
         console.log("No se le efectura el descuento.")
     }
 } else if (tarjeta == "frances"){
     if (acumulador >= 5000){
-        let descuento3 = acumulador*DESC3;
-        console.log ("El total de su compra es: $" + (acumulador) + ". Se le descuenta el 15%, abonará en total $" + (acumulador-descuento3));
+        calcularDescuento(DESC3);
+        console.log ("El total de su compra es: $" + (acumulador) + ". Se le descuenta el 15%, abonará en total $" + (total(calcularDescuento(DESC3))));
     }else{
         console.log("No se le efectura el descuento.")
     }
